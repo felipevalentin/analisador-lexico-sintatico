701352806263f557bf1d1aa640e89f49a391dcb3
Felipe Valentin <felipe.valentin@jusbrasil.com.br>
2022-11-15 14:22:41 -0300

    refactor regex parser

--- er.py
+++ er.py
@@ -1,115 +1,67 @@
-class ExpressaoRegular:
-    def __init__(self, expressao=None):
-        self.expressao = expressao
+class RegularExpression:
+    def __init__(self, re={}):
+        self.re = re
 
-    def ler_arquivo(self, nome):
-        with open(nome) as f:
+    def read_file(self, name):
+        with open(name) as f:
             for line in f:
-                head, tail = line.split(":")
+                def_re, raw_re = line.split(":")
+                self.re[def_re] = "".join(self.parse(raw_re))
 
-                if head == "er":
-                    self.er_normal(tail)
-                else:
-                    self.er_grupos(head, tail)
+    def parse(self, raw_re):
+        re = raw_re.strip().replace(" ", "")
+        re = self.parse_groups(re)
+        re = self.expand_extension(re)
+        re = self.add_and_symbol(re)
+        return re
 
-    def er_normal(self, tail):
-        er = tail.strip()
-        er = self.add_and(er)
-        er = self.add_parenteses(er)
-        er = self.expandir_extensoes(er)
-        self.expressao = er
-
-    def expandir_extensoes(self, tail):
-        er = []
-        ps = []
-        lp = None
-        for i, char in enumerate(tail):
-            last = len(er) - 1
-            if char == "(":
-                er.append("(")
-                ps.append(last + 1)
-            elif char == ")":
-                er.append(")")
-                lp = ps.pop()
-            elif char == "+":
-                er.append(".")
-                if lp is not None:
-                    er += er[lp : last + 1]
-                else:
-                    er.append(er[last])
-                er.append("*")
-            elif char == "?":
-                er.append("|")
-                er.append("&")
-            else:
-                er.append(char)
-        return er
-
-    def add_parenteses(self, tail):
-        er = []
-        ps = []
-        lp = None
-        for i, char in enumerate(tail):
-            last = len(er) - 1
-            if char == "(":
-                er.append("(")
-                ps.append(last + 1)
-            elif char == ")":
-                er.append(")")
-                lp = ps.pop()
-            elif char == "+":
-                if lp is not None:
-                    er.insert(lp, "(")
-                    er.append("+")
-                    er.append(")")
-                    lp = None
-                else:
-                    er.insert(last, "(")
-                    er.append("+")
-                    er.append(")")
-            elif char == "?":
-                if lp:
-                    er.insert(lp, "(")
-                    er.append("?")
-                    er.append(")")
-                    lp = None
-                else:
-                    er.insert(last, "(")
-                    er.append("?")
-                    er.append(")")
-            else:
-                er.append(char)
-        return er
-
-    def add_and(self, tail):
-        er = []
-        for i in range(len(tail)):
-            if (
-                i > 0
-                and tail[i] not in ["|", "+", "?", ")"]
-                and tail[i - 1] not in ["|", "("]
-            ):
-                er.insert(len(er), ".")
-                er.append(tail[i])
-            else:
-                er.append(tail[i])
-        return er
-
-    def er_grupos(self, head, tail):
-        start = None
-        finish = None
-        group = False
+    def parse_groups(self, raw_re):
+        re = raw_re
+        for k, v in self.re.items():
+            re = re.replace(k, v)
         lower = "|".join(chr(c) for c in range(ord("a"), ord("z") + 1))
         upper = "|".join(chr(c) for c in range(ord("A"), ord("Z") + 1))
         digit = "|".join(chr(c) for c in range(ord("0"), ord("9") + 1))
-        er = tail.replace("[a-z]", lower)
-        er = er.replace("[a-zA-Z]", lower + "|" + upper)
-        er = er.replace("[A-Z]", upper)
-        er = er.replace("[0-9]", digit)
-        self.expressao = er
+        re = re.replace("[a-z]", lower)
+        re = re.replace("[a-zA-Z]", lower + "|" + upper)
+        re = re.replace("[A-Z]", upper)
+        re = re.replace("[0-9]", digit)
+        return re
+
+    def expand_extension(self, raw_er):
+        re = []
+        for i in range(len(raw_er)):
+            if raw_er[i] == "?":
+                if raw_er[i - 1] == ")":
+                    re.insert("".join(re).rindex("("), "(")
+                else:
+                    re.insert(len(re) - 2, "(")
+                re += ["|", "&", ")"]
+            elif raw_er[i] == "+":
+                if raw_er[i - 1] == ")":
+                    re += re["".join(re).rindex("(") :]
+                else:
+                    re.append(re[-1])
+                re.append("*")
+            else:
+                re.append(raw_er[i])
+        return re
+
+    def add_and_symbol(self, raw_re):
+        re = []
+        for i in range(len(raw_re)):
+            if (
+                i > 1
+                and raw_re[i] not in ["|", ")", ".", "*"]
+                and raw_re[i - 1] not in ["|", "(", "."]
+            ):
+                re.append(".")
+            re.append(raw_re[i])
+        return re
 
 
 if __name__ == "__main__":
-    er = ExpressaoRegular()
-    er.ler_arquivo("entrada_er.txt")
-    print("".join(er.expressao))
+    re = RegularExpression()
+    re.read_file("entrada_er.txt")
+    for k, v in re.re.items():
+        print(f"{k}: {''.join(v)}")
