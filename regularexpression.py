7cfa05ee77318b8a433d0473c0e759a6ad7d6768
Felipe Valentin <felipe.valentin@jusbrasil.com.br>
2022-12-05 21:50:19 -0300

    fix er

--- regularexpression.py
+++ regularexpression.py
@@ -9,13 +9,13 @@ class Regex:
         re = self.parse_groups(re, parsed)
         re = self.create_regex_list(re)
         re = self.expand_extension(re)
-        re = self.add_and_symbol(re)
         return "".join(re)
 
     def parse_groups(self, raw_re, parsed={}):
         re = raw_re
         for parsed_re in parsed:
-            re = re.replace(parsed_re.def_re, parsed_re.re)
+            temp = "(" + parsed_re.re + ")"
+            re = re.replace(parsed_re.def_re, temp)
         lower = "|".join(chr(c) for c in range(ord("a"), ord("z") + 1))
         upper = "|".join(chr(c) for c in range(ord("A"), ord("Z") + 1))
         digit = "|".join(chr(c) for c in range(ord("0"), ord("9") + 1))
@@ -36,24 +36,13 @@ class Regex:
                 re += ["|", "&", ")"]
             elif raw_er[i] == "+":
                 if raw_er[i - 1] == ")":
-                    re += re["".join(re).rindex("(") :]
+                    re.append(".")
+                    re += re["".join(re).rindex("(") : -1]
                 else:
                     re.append(re[-1])
                 re.append("*")
             else:
                 re.append(raw_er[i])
-        return re
-
-    def add_and_symbol(self, raw_re):
-        re = []
-        for i in range(len(raw_re)):
-            if (
-                i > 0
-                and raw_re[i] not in ["|", ")", ".", "*"]
-                and raw_re[i - 1] not in ["|", "(", "."]
-            ):
-                re.append(".")
-            re.append(raw_re[i])
         return re
 
     def add_end_of_regex_symbol(self, raw_re):
@@ -63,20 +52,13 @@ class Regex:
         return re
 
     def create_regex_list(self, raw_re):
-        re = []
-        alf = ""
+        re = [*raw_re]
+        is_id = False
         for c in raw_re:
-            if c in ["(", ")", ".", "*", "|"]:
-                if alf != "":
-                    re.append(alf)
-                    if alf not in self.alphabet:
-                        self.alphabet.append(alf)
-                    alf = ""
-                re.append(c)
-            else:
-                alf = alf + c
-        if alf != "":
-            re.append(alf)
-            if alf not in self.alphabet:
-                self.alphabet.append(alf)
+            if c == "\\" and not is_id:
+                is_id = True
+            elif c not in ["(", ")", ".", "*", "|", "+", "?"] or is_id:
+                if c not in self.alphabet:
+                    self.alphabet.append(c)
+                is_id = False
         return re
--- syntaxtree.py
+++ syntaxtree.py
@@ -1,3 +1,7 @@
+import automata
+import regularexpression
+
+
 class Tree:
     def __init__(self, re):
         self.leaves = dict()
@@ -10,32 +14,36 @@ class Tree:
         tree = []
         operators = []
         leaf_id = 0
-
+        is_id = False
         for c in re:
-            if c == "(":
+            if c == "\\":
+                is_id = True
+            elif c == "(" and not is_id:
                 operators.append("(")
-            elif c == ")":
+            elif c == ")" and not is_id:
                 while operators[-1] != "(":
                     self.make_node(operators.pop(), tree)
                 operators.pop()
-            elif c in ["|", ".", "*"]:
+            elif c in ["|", ".", "*"] and not is_id:
                 while len(operators) > 0 and priority[operators[-1]] >= priority[c]:
                     self.make_node(operators.pop(), tree)
                 operators.append(c)
             else:
-                self.make_node(c, tree, leaf_id)
+                self.make_node(c, tree, True, leaf_id)
                 self.followpos.append(set())
                 self.leaves[leaf_id] = c
                 leaf_id += 1
+                is_id = False
 
         while len(tree) > 1:
             self.make_node(operators.pop(), tree)
 
         return tree[-1]
 
-    def make_node(self, label, tree, leaf_id=None):
+    def make_node(self, label, tree, is_id=False, leaf_id=None):
         root = {
             "leaf_id": leaf_id,
+            "is_id": True if is_id else False,
             "label": label,
             "left": None,
             "right": None,
@@ -56,28 +64,24 @@ class Tree:
         self.make_syntax_tree(node["left"])
         self.make_syntax_tree(node["right"])
 
-        if node["label"] == "|":
+        if node["label"] == "|" and not node["is_id"]:
             node["nullable"] = node["left"]["nullable"] or node["right"]["nullable"]
             node["firstpos"] = node["left"]["firstpos"].union(node["right"]["firstpos"])
             node["lastpos"] = node["left"]["lastpos"].union(node["right"]["lastpos"])
-        elif node["label"] == "*":
+        elif node["label"] == "*" and not node["is_id"]:
             node["nullable"] = True
             node["firstpos"] = node["left"]["firstpos"]
             node["lastpos"] = node["left"]["lastpos"]
             self.compute_follows(node)
-        elif node["label"] == ".":
+        elif node["label"] == "." and not node["is_id"]:
             node["nullable"] = node["left"]["nullable"] and node["right"]["nullable"]
             if node["left"]["nullable"]:
-                node["firstpos"] = node["left"]["firstpos"].union(
-                    node["right"]["firstpos"]
-                )
+                node["firstpos"] = node["left"]["firstpos"].union(node["right"]["firstpos"])
             else:
                 node["firstpos"] = node["left"]["firstpos"]
 
             if node["right"]["nullable"]:
-                node["lastpos"] = node["left"]["lastpos"].union(
-                    node["right"]["lastpos"]
-                )
+                node["lastpos"] = node["left"]["lastpos"].union(node["right"]["lastpos"])
             else:
                 node["lastpos"] = node["right"]["lastpos"]
             self.compute_follows(node)
@@ -113,3 +117,22 @@ class Tree:
                 # + str(node["lastpos"])
             )
             self.print_tree(node["left"], level + 1)
+
+
+def regex_from_read_file(name):
+    res = []
+    with open(name) as f:
+        for line in f:
+            def_re, raw_re = line.split(":")
+            res.append(regularexpression.Regex(def_re, raw_re, res))
+    return res
+
+
+regexs = regex_from_read_file("input/er.txt")
+for regex in regexs:
+    print(regex.re, regex.alphabet)
+    tree = Tree(regex.add_end_of_regex_symbol(regex.re))
+    dfa = automata.Automata(alfabeto=regex.alphabet)
+    dfa.re2dfa(tree)
+    dfa.def_re = regex.re
+    dfa.imprimir_atributos()
